43bf8c18eaf295bf9f0ae508d06bd8c2dc6201c2
Tom-254 <211mseeratiego@gmail.com>
2023-04-03 19:32:38 -0700

    0x01. Python - Async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""2-measure_runtime.py
+"""
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    measures the total execution time for
+    wait_n(n, max_delay), and returns total_time / n
+    """
+    time_started = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - time_started) / n
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""4-tasks.py
+"""
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    takes the code from wait_n and alter
+    it into a new function task_wait_n
+    """
+    delay_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(delay_times)
